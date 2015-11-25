efd40db4f87e1f5c3368ff3f0f25fced513d9d4e
Victor Darras <vdarras@synbioz.com>
2015-11-25 10:15:26 +0100

    fix context-menu submenu overflow (on bottom of page)

---
+++ javascripts/theme.js
@@ -0,0 +1,10 @@
+$(document).on('ready page:load', function () {
+  $('#context-menu').on('mouseenter', '.folder', function () {
+    var
+      $submenu = $(this).find('ul'),
+      overflow = window.innerHeight - $submenu.innerHeight() - $submenu.offset().top;
+    if ( overflow < 0 ) {
+      $submenu.css('margin-top', overflow);
+    }
+  });
+});
