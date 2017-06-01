783d7cf1bc2540a57bf07d6329c68c373c1d1196
Victor Darras <vdarras@synbioz.com>
2017-06-01 15:53:51 +0200

    Update copyToClipboard, add a related stylesheet

--- src/_copy_to_clipboard.js
+++ src/_copy_to_clipboard.js
@@ -1,48 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  var buttonStyles = `
-    .cpButton {
-      display:block!important;
-      position:absolute!important;
-      top:0!important;
-      right: 0!important;
-      padding:.4em .6em!important;
-      margin:1em!important;
-
-      font-size: .75em!important;
-      text-decoration:none!important;
-      text-transformation:uppercase!important;
-      line-height: 1!important;
-      color:white!important;
-
-      background:rgba(0,0,0,0.1)!important;
-      border-radius: 2em!important;
-      opacity: 0!important;
-      cursor:pointer!important;
-      transition: 0.25s!important;
-    }
-    .cpButton:not(.done):before {
-      content: 'COPIER'!important;
-    }
-    .cpButton.done:before {
-      content: 'COPIÉ'!important;
-    }
-    .cpButton:hover {
-      opacity: 1!important;
-      color: #fff!important;
-      background:rgba(0,0,0,0.8)!important;
-    }
-    .cpButton:active {
-      background:rgba(0,0,0,0.5)!important;
-    }
-    pre {
-      position:relative!important;
-    }
-    pre:hover .cpButton {
-      opacity:1!important;
-    }
-  `;
-
   function copyToClipboard() {
 
     let textArea = document.createElement('textarea');
@@ -55,11 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     this.classList.add('done');
   }
-
-  var style = document.createElement('style')
-  style.appendChild(document.createTextNode(buttonStyles));
-  style.type = 'text/css';
-  document.head.appendChild(style);
 
   var cpButton = document.createElement('a');
   cpButton.classList.add('cpButton');
